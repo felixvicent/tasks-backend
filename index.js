bd3a2d68d6ca62ae4ea163db5182cca3bb6f4b6e
Félix Vicente <felixvicent1306@gmail.com>
2020-07-14 15:00:58 -0300

    Config middlewares

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const consign = require('consign');
+
+const db = require('./config/db');
 
 const app = express();
+
+consign()
+  .then('./config/middlewares.js')
+  .into(app);
+
+app.db = db;
 
 app.get('/', (req, res) => {
   res.status(200).send('Menu backend');
